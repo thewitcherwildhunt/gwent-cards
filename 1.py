a674b119f6e8d087363fc2e871f6eafba1769f69
Mikhail Kurapov <kurapmikhail@gmail.com>
2024-02-01 21:45:36 +0300

    2

--- 1.py
+++ 1.py
@@ -3,5 +3,7 @@ import json
 
 r = requests.get('https://api.gwent.one/?key=data&version=11.7')
 data = json.loads(r.text)
-
-print(data['response']['0']['name'])
+for elem in data['response']:
+    for atr in ['id', 'attributes', 'name', 'category', 'ability', 'ability', 'ability_html', 'keyword_html', 'flavor']:
+        print(data['response'][elem][atr])
+    print()
