342619ca177c179d7f2532b1e66d76dd28325a3f
Mikhail Kurapov <kurapmikhail@gmail.com>
2024-02-01 22:25:01 +0300

    bs4

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
+response = urlopen('https://www.playgwent.com/ru/decks/')
+html = response.read().decode()
+soup = BeautifulSoup(html)
+for link in soup.find_all('a'):
+    print(link)
